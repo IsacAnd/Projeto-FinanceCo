513308ae67ce77dc07c25b8baaf1b8a10d17db80
IsacAnd <grimjaeguer@gmail.com>
2025-04-28 10:20:03 -0300

    Atualização do projeto
    Implementação inicial dos gráficos na página principal.
    Implementação de melhorias na responsividade.
    Implementação de melhorias nos designs das páginas.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (logoutBtn) {
     logoutBtn.addEventListener("click", () => {
-      window.location.href = "./pages/login.html";
+      window.location.href = "/pages/login.html";
     });
   }
 });
@@ -15,46 +15,77 @@ function login() {
   console.log(username, password);
 
   if (username == "isac" && password == "1234") {
-    window.location.href = "./index.html";
+    window.location.href = "../index.html";
+    showAlert("Login efetuado com sucesso!", "success");
+    return;
+  } else {
+    showAlert("Usuário ou senha incorretos!");
+    return;
   }
 }
 
 function register() {}
 
-// const ctx = document.getElementById('meuGrafico').getContext('2d');
+function showAlert(message, type = "error") {
+  const alertBox = document.getElementById("alert-message");
 
-//   // Dados iniciais do gráfico
-//   const data = {
-//     labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
-//     datasets: [{
-//       label: 'Vendas',
-//       data: [12, 19, 3, 5, 2, 3],
-//       borderColor: 'rgba(75, 192, 192, 1)',
-//       backgroundColor: 'rgba(75, 192, 192, 0.2)',
-//       tension: 0.4
-//     }]
-//   };
+  alertBox.textContent = message;
 
-//   // Opções
-//   const options = {
-//     responsive: true,
-//     animation: false,
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   };
+  if (type === "success") {
+    alertBox.classList.add("success");
+  } else {
+    alertBox.classList.remove("success");
+  }
 
-//   // Cria o gráfico
-//   const meuGrafico = new Chart(ctx, {
-//     type: 'line',
-//     data: data,
-//     options: options
-//   });
+  alertBox.classList.remove("hidden");
+  alertBox.classList.add("show");
 
-//   // Atualiza os dados a cada 3 segundos com valores aleatórios
-//   setInterval(() => {
-//     meuGrafico.data.datasets[0].data = Array.from({ length: 6 }, () => Math.floor(Math.random() * 20));
-//     meuGrafico.update();
-//   }, 3000);
+  setTimeout(() => {
+    alertBox.classList.remove("show");
+    setTimeout(() => {
+      alertBox.classList.add("hidden");
+    }, 500);
+  }, 3000);
+}
+
+const ctx = document.getElementById("meuGrafico").getContext("2d");
+
+// Dados iniciais do gráfico
+const data = {
+  labels: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "jul", "Ago"],
+  datasets: [
+    {
+      label: "Vendas",
+      data: [12, 19, 3, 5, 2, 3, 5, 10],
+      borderColor: "rgba(75, 192, 192, 1)",
+      backgroundColor: "rgba(75, 192, 192, 0.2)",
+      tension: 0.4,
+    },
+  ],
+};
+
+// Opções
+const options = {
+  responsive: true,
+  animation: false,
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
+
+// Cria o gráfico
+const meuGrafico = new Chart(ctx, {
+  type: "line",
+  data: data,
+  options: options,
+});
+
+// Atualiza os dados a cada 3 segundos com valores aleatórios
+setInterval(() => {
+  meuGrafico.data.datasets[0].data = Array.from({ length: 6 }, () =>
+    Math.floor(Math.random() * 20)
+  );
+  meuGrafico.update();
+}, 3000);
